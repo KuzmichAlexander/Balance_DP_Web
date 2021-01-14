4f28b4ea6695768dc83b0751b495cc76930958ad
ELKOYET <andrey_strelkov@mail.ru>
2021-01-14 17:12:52 +0500

    DB.2

--- balance_dp/balance_dp/ClientApp/src/DAl/api.js
+++ balance_dp/balance_dp/ClientApp/src/DAl/api.js
@@ -4,9 +4,10 @@ import {withMobileDialog} from "@material-ui/core";
 const baseURL = document.location.origin;
 
 export const getData = async () => { //Запрос на входные параметры
-    //const url = `${baseURL}/api/ThreadParams`;
-    //const {data} = await axios.get(url);
-    return inputParams;
+    const url = `${baseURL}/api/ThreadParams`;
+    const { data } = await axios.get(url);
+    console.log(data);
+    return data;
 }
 
 export const fetchData = async (params) => { // Отправляем входные, получаем результат
